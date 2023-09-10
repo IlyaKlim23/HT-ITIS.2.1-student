ee1a9e087ebaaebc1ba69b161ac1490cbc02fa76
ilklmkn <klimkin_ilya2004@mail.ru>
2023-09-10 12:43:31 +0300

    test

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -2,12 +2,26 @@
 
 public static class Parser
 {
-    public static void ParseCalcArguments(string[] args, 
-        out double val1, 
-        out CalculatorOperation operation, 
+    //hhh
+    public static void ParseCalcArguments(string[] args,
+        out double val1,
+        out CalculatorOperation operation,
         out double val2)
     {
-        throw new NotImplementedException();
+        if (!double.TryParse(args[0], out val1) || !double.TryParse(args[2], out val2) ||
+            args.Length != 3)
+        {
+            throw new ArgumentException();
+        }
+
+        operation = args[1] switch
+        {
+            "+" => CalculatorOperation.Plus,
+            "-" => CalculatorOperation.Minus,
+            "*" => CalculatorOperation.Multiply,
+            "/" => CalculatorOperation.Divide,
+            _ => throw new InvalidOperationException()
+        };
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
